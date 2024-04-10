c15d52e2c645914989cc062c118a4b2da5cc5194
Raphael Eze Echimina <raphaeleze@gmail.com>
2024-04-10 18:13:34 +0200

    Update and rename variables.tf to Modules/Aws Ec2/ variables.tf

--- Modules/Aws Ec2/ variables.tf
+++ Modules/Aws Ec2/ variables.tf
@@ -2,10 +2,12 @@ variable "awsRegion" {
   type    = string
   default = "us-east-1"
 }
+
 variable "ami" {
   type = string
   default = "ami-0f403e3180720dd7e"  
 }
+
 variable "instance_type" {
   type = string
   default = "t2.micro"
